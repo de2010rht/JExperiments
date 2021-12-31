4e1e84e14f952161f7ac5c1ae618c62bf0228793
de2010rht <de.rht2010@gmail.com>
2021-12-31 22:48:21 +0530

    Deitel Basic File & Directory Info

---
+++ src/com/pearson/deitel/io/basic/FileAndDirectory.java
@@ -0,0 +1,57 @@
+package com.pearson.deitel.io.basic;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class FileAndDirectory {
+	/**
+	 * Path interface
+	 * -	Represent the location of a file or directory
+	 * -	Do not open files or provide any file-processing capabilities
+	 * Paths class
+	 * -	static methods used to get Path objects representing file or directory locations
+	 * Files class
+	 * -	static methods for common file and directory manipulations
+	 * DirectoryStream interface
+	 * -	Iterate through the contents of a directory
+	 */
+	
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter file or directory name:");
+		Path path = Paths.get(scanner.nextLine());
+		
+		if (Files.exists(path)) {
+			System.out.printf("%n%s exists%n", path.getFileName());
+			System.out.printf("%s a directory%n", Files.isDirectory(path) ? "Is" : "Is not");
+			System.out.printf("%s an absolute path%n", path.isAbsolute() ? "Is" : "Is not");
+			System.out.printf("Last modified: %s%n", Files.getLastModifiedTime(path));
+			System.out.printf("Size: %s%n", Files.size(path));
+			System.out.printf("Path: %s%n", path);
+			System.out.printf("Absolute path: %s%n", path.toAbsolutePath());
+			
+			if (Files.isDirectory(path)) {
+				System.out.printf("%nDirectory contents:%n");
+				
+				// object for iterating through a directory's contents
+				DirectoryStream<Path> stream = Files.newDirectoryStream(path);
+				for (Path p : stream)
+					System.out.println(p);
+			}
+		} else
+			System.out.printf("%s does not exist%n", path);
+		
+		scanner.close();
+	}
+
+}
